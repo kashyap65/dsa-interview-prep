ad5a11871b27db4e0d3b95c5f44d8dc7d56d5e04
Kashyap Pandey <kashyappandey495@gmail.com>
2026-03-09 01:08:46 +0530

    git commit -m "✅ [linked_list] LinkedListCycle — Medium — Mar 5"

---
+++ src/main/java/com/kashyap/dsa/linked_list/ListNode.java
@@ -0,0 +1,11 @@
+package com.kashyap.dsa.linked_list;
+
+public class ListNode {
+    int data;
+    ListNode next;
+
+    public ListNode(int data, ListNode next) {
+        this.data = data;
+        this.next = next;
+    }
+}
